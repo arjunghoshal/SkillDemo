423b06fb5ae25072d805f6d4e062d5550fd372dc
Arjun Ghoshal <arjunp.ghoshal@gmail.com>
2022-02-04 14:35:51 -0800

    Added libraries and failing test

---
+++ SkillDemoTest.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemoTest {
+    @Test
+    public void multiplyTest()
+    {
+        assertEquals(16, SkillDemo.multiply(4, 3));
+    }
+}
